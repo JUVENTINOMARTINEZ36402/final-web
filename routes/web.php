6e74f9c4692a4e892b2b1b392c85c37b96ae4b82
dafemz <dmunoz35191@ucq.edu.mx>
2024-11-29 00:06:21 -0600

    commit inicial

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\EventController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/user', [UserController::class, 'index']);
+
+Route::resource('events', EventController::class);
